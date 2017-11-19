664e4083357df80f9f11613b2e178a6c0281466b
dbolo <31314227+dbolo@users.noreply.github.com>
2017-11-18 18:07:58 -0600

    Website under construction
    Website under construction

---
+++ map.js
@@ -0,0 +1,22 @@
+      //6.4281° N, 9.4295° W
+      //6.3814386,-11.0029865
+        var map;
+        var liberia = {lat: 6.4281, lng: -9.0029865}
+        
+
+        
+        function initMap() {
+          map = new google.maps.Map(document.getElementById('map'), {
+            center: liberia,
+            zoom: 8
+          });
+          
+           var marker = new google.maps.Marker({
+            position: liberia,
+            map: map,
+            animation: google.maps.Animation.BOUNCE,
+            icon: 'marker-256.png'
+
+          });
+
+        }
